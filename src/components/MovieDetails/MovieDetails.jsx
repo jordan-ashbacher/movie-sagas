ebb50d4dd697a1eb45aa6328e0248f6bfd63a7a6
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-01-30 14:24:12 -0600

    styles detail button

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -3,9 +3,9 @@ import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import Card from "@material-ui/core/Card"
 import CardActionArea from "@material-ui/core/CardActionArea"
-import CardContent from "@material-ui/core/CardContent"
 import CardMedia from "@material-ui/core/CardMedia"
-import Typography from "@material-ui/core/Typography"
+import Button from '@material-ui/core/Button'
+
 
 const MovieDetail = () => {
   const history = useHistory();
@@ -32,7 +32,7 @@ const MovieDetail = () => {
         })}
       </ul>
       <p className="movieDescription">{movie.description}</p>
-      <button onClick={handleClick}>Back to List</button>
+      <Button variant="outlined" onClick={handleClick}>Back to List</Button>
     </div>
   );
 };
