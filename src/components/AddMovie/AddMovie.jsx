47b6717984200d92889f3efa3eb3b2d8b670b457
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-01-29 19:57:07 -0600

    creates inputs in AddMovie

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,9 +1,56 @@
 import '../AddMovie/AddMovie.css'
+import {useState} from 'react'
 
 const AddMovie = () => {
 
+    const [newMovie, setNewMovie] = useState({
+        title: '',
+        poster: '',
+        description: '',
+        genre_id: ''
+    })
+
     return (
-        <h1>ADD MOVIE</h1>
+        <form action="">
+            <input 
+                type="text"
+                placeholder="Title"
+                value={newMovie.title}
+                onChange={handleChange}
+                required
+            />
+            <input 
+                type="text"
+                placeholder="Poster URL"
+                value={newMovie.poster}
+                onChange={handleChange}
+                required
+            />
+            <input 
+                type="textarea"
+                placeholder="Description"
+                value={newMovie.description}
+                onChange={handleChange}
+                required
+            />
+            <label for="genre">Genre:</label>
+            <select name="genre" id="genre">
+                <option value="Adventure">Adventure</option>
+                <option value="Animated">Animated</option>
+                <option value="Biographical">Biographical</option>
+                <option value="Comedy">Comedy</option>
+                <option value="Disaster">Disaster</option>
+                <option value="DramaEpic">DramaEpic</option>
+                <option value="Epic">Epic</option>
+                <option value="Fantasy">Fantasy</option>
+                <option value="Musical">Musical</option>
+                <option value="Romantic">Romantic</option>
+                <option value="Science Fiction">Science Fiction</option>
+                <option value="Space-Opera">Space-Opera</option>
+                <option value="Superhero">Superhero</option>
+            </select>
+        </form>
+        
     )
 }
 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,33 +1,27 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom'
-import './MovieList.css'
-import MovieItem from '../MovieItem/MovieItem'
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useHistory } from "react-router-dom"
+import "./MovieList.css"
+import MovieItem from "../MovieItem/MovieItem"
 
 function MovieList() {
+  const dispatch = useDispatch()
+  const movies = useSelector((store) => store.movies)
 
-    
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+  useEffect(() => {
+    dispatch({ type: "FETCH_MOVIES" })
+  }, [])
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
-
-
-    return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <MovieItem key={movie.id} movie={movie} />
-                    );
-                })}
-            </section>
-        </main>
-
-    );
+  return (
+    <main>
+      <h1>MovieList</h1>
+      <section className="movies">
+        {movies.map((movie) => {
+          return <MovieItem key={movie.id} movie={movie} />
+        })}
+      </section>
+    </main>
+  )
 }
 
-export default MovieList;
+export default MovieList
